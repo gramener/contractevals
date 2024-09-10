ee8a1789b803f3c8d6178bb98e5da1bf17b80bb7
S Anand <root.node@gmail.com>
2024-09-10 22:16:16 +0800

    ENH: UI enhancements
    1. Show ISBN and search by ISBN
    2. Remove ? in headers - doesn't look nice
    3. Ensure ISBNs are fixed numbers, not 9E+12...
    4. Replace circle/cross unicode with Bootstrap icons

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import { html, render } from "https://cdn.jsdelivr.net/npm/lit-html@2.7.5/lit-html.min.js";
+import { unsafeHTML } from "https://cdn.jsdelivr.net/npm/lit-html@3/directives/unsafe-html.js";
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 import Fuse from "https://cdn.jsdelivr.net/npm/fuse.js@6.6.2/dist/fuse.esm.js";
 
@@ -25,26 +26,6 @@ async function handleFileUpload() {
         const parsedDate = new Date(row[field]);
         row[field] = !isNaN(parsedDate) ? parsedDate : row[field];
       });
-
-      // Process the "Is agreement fully signed by all parties?" field
-      const signedStatus = row["Is agreement fully signed by all parties?"].trim();
-      row["signed"] = signedStatus.match(/Fully signed/i)
-        ? "✅"
-        : signedStatus.match(/Not signed/i) || signedStatus === ""
-        ? "❌"
-        : "🟡";
-      row["recommendation"] = row["Recommendation for Licensing?"].match(/\bYes\b/i)
-        ? "🟢"
-        : row["Recommendation for Licensing?"].match(/\bNo\b/i)
-        ? "🔴"
-        : "🟡";
-
-      row["rights-grant"] =
-        row['Type of Rights Grant, Part 1 ("copyright assignment", "copyright transfer or license", "other")'] +
-        " " +
-        row['Type of Rights Grant, Part 2: "Exclusive" or "Non-Exclusive" or Silent'] +
-        row['Type of Rights Grant, Part 3: "Revocable" or "Irrevocable" or Silent'];
-
       return row;
     });
 
@@ -58,24 +39,46 @@ async function handleFileUpload() {
       "Contract Signing Date",
       "Latest Signing Date",
       "Agreement Date",
-      "Is agreement fully signed by all parties?",
-      "Recommendation for Licensing?",
+      "Is agreement fully signed by all parties",
+      "Recommendation for Licensing",
       "Rationale",
-      "Type of Rights Grant, Part 1 (\"copyright assignment\", \"copyright transfer or license\", \"other\")",
-      "Type of Rights Grant, Part 2: \"Exclusive\" or \"Non-Exclusive\" or Silent",
-      "Type of Rights Grant, Part 3: \"Revocable\" or \"Irrevocable\" or Silent",
+      'Type of Rights Grant, Part 1 ("copyright assignment", "copyright transfer or license", "other")',
+      'Type of Rights Grant, Part 2: "Exclusive" or "Non-Exclusive" or Silent',
+      'Type of Rights Grant, Part 3: "Revocable" or "Irrevocable" or Silent',
       "Governing Law (Country or State only)",
-      "Term of Agreement (\"Full Term of Copyright\", or Language if \"Other\")",
-      "Amendments/Appendices (Y/N?)",
-      "Summary of Amendment/Appendix"
+      'Term of Agreement ("Full Term of Copyright", or Language if "Other")',
+      "Amendments/Appendices (Y/N)",
+      "Summary of Amendment/Appendix",
     ];
     const missingFields = requiredFields.filter((field) => !csvData.some((row) => row[field]));
     if (missingFields.length > 0) {
       return displayFeedback(`Missing required fields: ${missingFields.join(", ")}.`, "danger");
     }
 
+    for (const row of csvData) {
+      // Process the "Is agreement fully signed by all parties?" field
+      const signedStatus = row["Is agreement fully signed by all parties"].trim();
+      row["signed"] = signedStatus.match(/Fully signed/i)
+        ? html`<i class="bi bi-check-square-fill text-success"></i>`
+        : signedStatus.match(/Not signed/i) || signedStatus === ""
+        ? html`<i class="bi bi-x-square-fill text-danger"></i>`
+        : html`<i class="bi bi-circle-fill text-warning"></i>`;
+      row["recommendation"] = row["Recommendation for Licensing"].match(/\bYes\b/i)
+        ? html`<i class="bi bi-circle-fill text-success"></i>`
+        : row["Recommendation for Licensing"].match(/\bNo\b/i)
+        ? html`<i class="bi bi-circle-fill text-danger"></i>`
+        : html`<i class="bi bi-circle-fill text-warning"></i>`;
+
+      row["rights-grant"] =
+        row['Type of Rights Grant, Part 1 ("copyright assignment", "copyright transfer or license", "other")'] +
+        " " +
+        row['Type of Rights Grant, Part 2: "Exclusive" or "Non-Exclusive" or Silent'] +
+        row['Type of Rights Grant, Part 3: "Revocable" or "Irrevocable" or Silent'];
+      row["ISBN"] = row["ISBN"].replace(/^B/, "");
+    }
+
     // Process the CSV data here
-    renderTable(csvData.filter((row) => row["Recommendation for Licensing?"]));
+    renderTable(csvData.filter((row) => row["Recommendation for Licensing"]));
     displayFeedback("File uploaded and processed successfully!", "success");
   } catch (error) {
     console.error(error);
@@ -90,7 +93,7 @@ function renderTable(data) {
   let sortColumn = null;
   let sortDirection = 1;
   let fuse = new Fuse(data, {
-    keys: ["Signatory Name", "Signatory Location"],
+    keys: ["ISBN", "Signatory", "rights-grant", "Governing Law (Country or State only)"],
     threshold: 0.4,
   });
 
@@ -125,11 +128,14 @@ function renderTable(data) {
   };
 
   const tableTemplate = (data) => html`
-    <input type="text" class="form-control" placeholder="Search signatories or locations" @input=${searchData} />
+    <input type="text" class="form-control" placeholder="Search by ISBN" @input=${searchData} />
     <div class="table-responsive">
       <table class="table table-striped table-hover">
         <thead>
           <tr>
+            <th @click=${() => sortData("ISBN")}>
+              ISBN ${sortColumn === "ISBN" ? (sortDirection > 0 ? "▲" : "▼") : ""}
+            </th>
             <th @click=${() => sortData("Signatory Name")}>
               Signatory ${sortColumn === "Signatory Name" ? (sortDirection > 0 ? "▲" : "▼") : ""}
             </th>
@@ -137,13 +143,13 @@ function renderTable(data) {
               Date ${sortColumn === "Agreement Date" ? (sortDirection > 0 ? "▲" : "▼") : ""}
             </th>
             <th class="text-center" @click=${() => sortData("signed")}>
-              Signed? ${sortColumn === "signed" ? (sortDirection > 0 ? "▲" : "▼") : ""}
+              Signed ${sortColumn === "signed" ? (sortDirection > 0 ? "▲" : "▼") : ""}
             </th>
             <th @click=${() => sortData("recommendation")}>
               Reco ${sortColumn === "recommendation" ? (sortDirection > 0 ? "▲" : "▼") : ""}
             </th>
             <th @click=${() => sortData("Rationale")}>
-              Why? ${sortColumn === "Rationale" ? (sortDirection > 0 ? "▲" : "▼") : ""}
+              Why ${sortColumn === "Rationale" ? (sortDirection > 0 ? "▲" : "▼") : ""}
             </th>
             <th @click=${() => sortData("rights-grant")}>
               Grant ${sortColumn === "rights-grant" ? (sortDirection > 0 ? "▲" : "▼") : ""}
@@ -160,8 +166,8 @@ function renderTable(data) {
                   : "▼"
                 : ""}
             </th>
-            <th @click=${() => sortData("Amendments/Appendices (Y/N?)")}>
-              Amendments ${sortColumn === "Amendments/Appendices (Y/N?)" ? (sortDirection > 0 ? "▲" : "▼") : ""}
+            <th @click=${() => sortData("Amendments/Appendices (Y/N)")}>
+              Amendments ${sortColumn === "Amendments/Appendices (Y/N)" ? (sortDirection > 0 ? "▲" : "▼") : ""}
             </th>
           </tr>
         </thead>
@@ -169,22 +175,19 @@ function renderTable(data) {
           ${data.map(
             (row, i) => html`
               <tr data-id="${i}" @click=${() => showContractDetails(row)}>
+                <td><a href="${row["eLIB link"]}" target="_blank">${row["ISBN"]}</a></td>
                 <td>
-                  <a href="${row["eLIB link"]}" target="_blank" title="ISBN: ${row["ISBN"]}"
-                    >${row["Signatory Name"]}</a
-                  >
+                  ${row["Signatory Name"]}
                   <div class="small">${row["Signatory Location"]}</div>
                 </td>
                 <td class="text-end">${formatDate(row["Agreement Date"])}</td>
-                <td class="text-center" title="${row["Is agreement fully signed by all parties?"]}">
-                  ${row["signed"]}
-                </td>
-                <td title="${row["Recommendation for Licensing?"]}">${row["recommendation"]}</td>
+                <td class="text-center" title="${row["Is agreement fully signed by all parties"]}">${row["signed"]}</td>
+                <td title="${row["Recommendation for Licensing"]}">${row["recommendation"]}</td>
                 <td>${row["Rationale"]}</td>
                 <td>${row["rights-grant"]}</td>
                 <td>${shorten(row["Governing Law (Country or State only)"], 30)}</td>
                 <td>${shorten(row['Term of Agreement ("Full Term of Copyright", or Language if "Other")'], 30)}</td>
-                <td>${row["Amendments/Appendices (Y/N?)"]} ${shorten(row["Summary of Amendment/Appendix"], 100)}</td>
+                <td>${row["Amendments/Appendices (Y/N)"]} ${shorten(row["Summary of Amendment/Appendix"], 100)}</td>
               </tr>
             `
           )}
@@ -200,21 +203,23 @@ let currentRowIndex = -1;
 
 function showContractDetails(row) {
   console.log(row);
-  currentRowIndex = filteredData.findIndex(r => r === row);
+  currentRowIndex = filteredData.findIndex((r) => r === row);
   updateModalContent(row);
 }
 
 function updateModalContent(row) {
-  $modalTitle.textContent = `Contract Details for ${row['Signatory Name']}`;
+  $modalTitle.textContent = `Contract Details for ${row["Signatory Name"]}`;
 
   const tableContent = Object.entries(row)
-    .map(([key, value]) => `
+    .map(
+      ([key, value]) => `
       <tr>
         <th scope="row">${key}</th>
         <td>${value instanceof Date ? formatDate(value) : value}</td>
       </tr>
-    `)
-    .join('');
+    `
+    )
+    .join("");
 
   $modalBody.innerHTML = `
     <div class="table-responsive">
@@ -229,18 +234,18 @@ function updateModalContent(row) {
   if (!$contractModal.classList.contains("show")) modal.show();
 }
 
-document.addEventListener('keydown', handleKeyPress);
+document.addEventListener("keydown", handleKeyPress);
 
 function handleKeyPress(event) {
   if (!$contractModal.classList.contains("show")) return;
 
   switch (event.key) {
-    case 'ArrowLeft':
-    case 'ArrowUp':
+    case "ArrowLeft":
+    case "ArrowUp":
       navigateModal(-1);
       break;
-    case 'ArrowRight':
-    case 'ArrowDown':
+    case "ArrowRight":
+    case "ArrowDown":
       navigateModal(1);
       break;
   }
